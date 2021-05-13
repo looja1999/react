8db70add0cb83dbc704a063c1eb3dc63322ad356
Looja <looja1999@gmail.com>
2021-05-14 09:37:11 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TestComponents from "./Components/TestComponents";
+
+function App() {
+  return (
+    <div className="App">
+      <TestComponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TestComponents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestComponents = () => {
+  return <input type="button" value="Click"></input>;
+};
+
+export default TestComponents;
